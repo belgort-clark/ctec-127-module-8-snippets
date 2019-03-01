27afe47974d170e1700859bc0e00002afd693d49
belgort-clark <belgort@clark.edu>
2019-02-28 16:58:40 -0800

    Updated to include Bootstrap

--- language-search.php
+++ language-search.php
@@ -4,48 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>World Search Demo </title>
 </head>
 <body>
-    <?php require_once 'inc/mysqli_connect.inc.php'; ?>
-    <?php 
-    $sql = 'SELECT DISTINCT CountryCode FROM countrylanguage';
-    $result = $db->query($sql); 
-    ?>
-    <h1>Countries by Language</h1>
-    <form action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST">
-        <label for="country">Select a Country
-            <select name="country" id="country">
-                <?php 
-                    while($row = $result->fetch_assoc()){
-                        echo "<option value=\"" . $row['CountryCode'] ."\">" . $row['CountryCode'] . "</option>\n";
-                    }
+
+<?php
+require_once 'inc/mysqli_connect.inc.php';
+$sql = 'SELECT DISTINCT CountryCode FROM countrylanguage';
+$result = $db->query($sql);
+
+// for sticky select
+$country = '';
+$selected = '';
+
+if (isset($_POST['country'])) {
+    $country = $_POST['country'];
+}
+
+?>
+
+<div class="container">
+    <div class="col-12 mt-5">
+        <h1>Countries by Language</h1>
+        <p class="mb-5">CTEC 127 - Winter 2019</p>
+        <form action="<?=htmlspecialchars($_SERVER['PHP_SELF'])?>" method="POST" class="mb-5">
+            <div class="form-group mb-4">
+                <label for="country" class="form-label mb-3">Select a Country</label>
+                    <select class="form-control w-25" name="country" id="country">
+                        <?php
+                        // Loop through each row in the result set
+                        while ($row = $result->fetch_assoc()) {
+                            // sticky select check
+                            if ($row['CountryCode'] == $country) {
+                                $selected = ' selected';
+                            }
+                            echo "<option value=\"" . $row['CountryCode'] . "\" $selected>" . $row['CountryCode'] . "</option>\n";
+                            $selected = '';
+                        }
+                        ?>
+                    </select>
+            </div>
+            <input type="submit" value="Search" class="btn btn-primary mb-2">
+        </form>
+
+        <?php
+        // Code to display search results
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $sql = "SELECT * FROM countrylanguage WHERE CountryCode=" . '"' . $_POST["country"] . '"';
+            $result = $db->query($sql);
+
+            if ($result->num_rows > 0) {
+                echo "<h3 class='mb-4'>Results for {$_POST["country"]}</h3>";
                 ?>
-            </select>
-            <input type="submit" value="Search">
-        </label>
-    </form>
+                <table class="table table-hover">
+                    <thead class="thead-dark"><tr><th>Language</th><th>Official Language</th><th>Percentage</th></tr></thead>
+                    <?php 
+                    // Code to change showing T / F to Yes / No
+                    while ($row = $result->fetch_assoc()) {
+                        echo "<tr>";
+                        if ($row["IsOfficial"] == 'T') {
+                            $official = 'Yes';
+                        } else {
+                            $official = 'No';
+                        }
 
-    <?php 
-    /*
-    Table Column Names
-    - CountryCode
-    - Language
-    - IsOfficial
-    - Percentage
-    */
+                        // Format the percetage using sprintf. Go research this function    
+                        $percentage = sprintf("%.2f%%", $row["Percentage"]);
 
-    // Code to display search results
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        // build SQL
-        $sql = "SELECT * FROM countrylanguage WHERE CountryCode=" . '"' . $_POST["country"] . '"';
-        $result = $db->query($sql);
-        while ($row = $result->fetch_assoc()) {
-            echo "<div>";
-            echo $row["CountryCode"] . "---" . $row["Language"];
-            echo "</div>";
+                        echo "<td>{$row["Language"]}</td><td>{$official}</td><td>{$percentage}</td>";
+                        echo "</tr>";
+                    }    
+                    ?>
+                </table>
+                <?php
+            }
         }
-    }
-    ?>
+        ?>
+    </div>
+</div>
 </body>
 </html>
